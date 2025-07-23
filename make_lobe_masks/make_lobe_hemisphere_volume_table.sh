74740ff9ce4cb8ade312f6cd494bcf310e282296
Neva <nevao@uw.edu>
2025-07-23 13:31:42 -0700

    added comment about usage

--- make_lobe_masks/make_lobe_hemisphere_volume_table.sh
+++ make_lobe_masks/make_lobe_hemisphere_volume_table.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Note that this file will calculate volumes based on summing of masks. However, it will yield different values than 
+# those produced by aparcstats2table and asegstats2table because those functions take into account partial volume effects
+# or corrections. There may also be small gaps or extra voxels at the border that are not included in those functions.
+# For subcortical regions, I find that the difference between my program here and the output of asegstats2table is about 2.5%.
+# I prefer to sum the values to those tables than use the values calculated here.
+
 SUBJECTS_DIR="/home/toddr/neva/MPF/make_lobe_masks/combined_masks"
 SUBJECTS_LIST="subjects_list.txt"
 REGIONS_LIST="regions.txt"

4b2167e140b7be3eadc9e788b04ca74fa842006c
nevacorr <nevao@uw.edu>
2025-09-05 13:45:56 -0700

    make general filenames

--- overlay_parcellations_mpf_mprage/register_parcellations.sh
+++ overlay_parcellations_mpf_mprage/register_parcellations.sh
@@ -1,19 +1,36 @@
-cp register_parcellations.sh register_parcellations.sh.bak 
+#!/bin/bash
 
-mkdir -p output
+# This program registers MPF imaages to mprage
+
+# Usage: bash register_parcellations.sh <mpf_dir> <mprage_dir>
+# Example: bash register_parcellations.sh H05-2_MPFcor_freesurfer H05-2_mprage1_freesurfer
+
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 <mpf_dir> <mprage_dir>"
+	exit 1
+fi
+
+MPF_DIR=$1
+MPRAGE_DIR=$2
+OUTPUT_DIR="output"
+
+mkdir -p "$OUTPUT_DIR"
+
+# Strip "_freesurfer" suffix if present to use in filenames
+BASE_NAME=$(basename "$MPF_DIR" _freesurfer)
 
 # Register MPF brain to mprage brain. Create a registration transform
 mri_robust_register \
-	  --mov H05-2_MPFcor_freesurfer/mri/brain.mgz \
-	  --dst H05-2_mprage1_freesurfer/mri/brain.mgz \
-	  --lta output/H05-2_MPFcor_to_mprage.lta \
+	  --mov "${MPF_DIR}/mri/brain.mgz" \
+	  --dst "${MPRAGE_DIR}/mri/brain.mgz" \
+	  --lta "${OUTPUT_DIR}/${BASE_NAME}_to_mprage.lta" \
           --satit \
-	  --mapmov output/brain_H05-2_MPFcor_in_mprage.mgz
+	  --mapmov "${OUTPUT_DIR}/brain_${BASE_NAME}_in_mprage.mgz"
 
 # Resample MPF aparc+aseg into mprage space using transform
 mri_vol2vol  \
-	   --mov H05-2_MPFcor_freesurfer/mri/aparc+aseg.mgz \
-	    --targ H05-2_mprage1_freesurfer/mri/aparc+aseg.mgz \
-            --lta output/H05-2_MPFcor_to_mprage.lta \
-            --o output/H05-2_MPFcor_aparc+aseg_reg_to_mprage.mgz  --nearest  
+	   --mov "${MPF_DIR}/mri/aparc+aseg.mgz" \
+	    --targ "${MPRAGE_DIR}/mri/aparc+aseg.mgz" \
+            --lta "${OUTPUT_DIR}/${BASE_NAME}_to_mprage.lta" \
+            --o "${OUTPUT_DIR}/${BASE_NAME}_aparc+aseg_reg_to_mprage.mgz"  --nearest  
 

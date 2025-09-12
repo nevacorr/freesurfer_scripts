980d7173b71c8c9b69954da2d353150159cb633b
nevacorr <nevao@uw.edu>
2025-09-12 13:08:44 -0700

    output voxel counts for changes between MPF and MPRAGE

--- overlay_parcellations_mpf_mprage/compare_parcellations_full_brain.py
+++ overlay_parcellations_mpf_mprage/compare_parcellations_full_brain.py
@@ -17,31 +17,6 @@ mprage_name = os.path.basename(mprage_path).replace('.mgz', '')
 mpf_name = os.path.basename(mpf_path).replace('.mgz', '')
 output_file = os.path.join(output_dir, f'diff_{mprage_name}_vs_{mpf_name}.mgz')
 
-lut_file = os.path.join(os.environ['FREESURFER_HOME'], 'FreeSurferColorLUT.txt')
-exclude_keywords = ['unknown', 'wm-', 'unused', 'long', 'short', 'part', 'granular', 'layer']
-
-subcortical_label_nums = [10, 11, 12, 13, 49, 50, 51, 52]
-cortical_ranges = [(1000,1036), (2000,2036)]
-
-label_dict = {}
-with open(lut_file) as f:
-	for line in f:
-		if line.startswith('#') or not line.strip():
-			continue
-		parts = line.strip().split()
-		label_num = int(parts[0])
-		label_name = parts[1]
-
-		if any(ex in label_name for ex in exclude_keywords):
-			continue
-
-		if label_num in subcortical_label_nums:
-			label_dict[label_num] = label_name
-			continue
-
-		if any(start <= label_num <= end for (start,end) in cortical_ranges):
-			label_dict[label_num] = label_name
-
 # Load parcellations
 img1 = nib.load(mprage_path)
 img2 = nib.load(mpf_path)
@@ -51,36 +26,71 @@ data2 = img2.get_fdata().astype(np.int32)
 # Initialize diff volume
 diff_volume = np.zeros(data1.shape, dtype=np.int16)
 
-# Voxels in image 1 parcellation but not in or with different label in image 2 parcellation
-image1_only = (data1 != 0) & (data2 != data1)
-diff_volume[image1_only] = 1
+# Labels
+wm_labels = [2, 41]
+gm_labels = list(range(1000, 1036)) + list(range(2000, 2036)) #cortical
+gm_labels += [10, 11, 12, 13, 49, 50, 51, 52] # subcortical
+csf_labels = [4, 43, 14, 15, 24, 31, 63]
 
-# Voxels in image 2 parcellation but not in or with different label in image 1 parcellation
-image2_only = (data2 != 0) & (data2 != data1)
-diff_volume[image2_only] = 2
+# Masks
+wm_mask1 = np.isin(data1, wm_labels)
+wm_mask2 = np.isin(data2, wm_labels)
+csf_mask1 = np.isin(data1, csf_labels)
+csf_mask2 = np.isin(data2, csf_labels)
 
-# Voxels in both parcellations but assigned to different parcels
-different_label = (data1 != 0) & (data2 != 0) & (data1 != data2)
-diff_volume[different_label] = 3
-
-# Compute differences
-for label_num, region_name in label_dict.items():
-
-	if label_num == 0:
-		continue
+def classify(label):
+	if label in wm_labels:
+		return 1 # WM
+	elif label in gm_labels:
+		return 2 # GM
+	elif label in csf_labels:
+		return 3 # CSF
+	else:
+		return 0 # ignore, background or non-interest	
 	
-	mask1 = (data1 == label_num)
-	mask2 = (data2 == label_num)
-	voxels_img1 = mask1.sum()
-	voxels_img2 = mask2.sum()
-	voxels_diff = np.logical_xor(mask1, mask2).sum()
-	
-	print(f'{region_name}: numvox_mprage={voxels_img1} numvox_mpf={voxels_img2}, num differing voxels={voxels_diff}')
 
-# Save as .mgz
+# Map voxel labels into WM/ GM/ CSF/ other
+class1 = np.vectorize(classify)(data1)
+class2 = np.vectorize(classify)(data2)
+
+# Build difference map
+diff_volume = np.zeros(data1.shape, dtype=np.int16)
+
+# WM in img1 to GM in img2
+diff_volume[(class1 == 1) & (class2 == 2)] = 1
+# GM in img1 to WM in img2
+diff_volume[(class1 == 2) & (class2 == 1)] = 2
+# CSF in img1 to WM or GM in img2
+diff_volume[(class1 == 3) & ((class2 == 1) | (class2 == 2))] = 3
+# WM or GM in img1, to CSF in img2 
+diff_volume[((class1 == 1) | (class1 == 2)) & (class2 == 3)] = 4
+
+# Make categories variable for calculating sum for each category
+categories = {
+	1: "WM in MPRAGE - GM in MPF", 
+	2: "GM in MPRAGE - WM in MPF",
+	3: "CSF in MPRAGE - GM/WM in MPF",
+	4: "GM/WM in MPRAGE - CSF in MPF"
+}
+
+# Construct counts text file name
+counts_file_name = os.path.join(
+	os.path.dirname(output_file), 												'count_' + os.path.basename(output_file).replace('.mgz', '.txt')
+)
+
+# Write counts to file
+with open(counts_file_name, 'w') as f:
+	f.write("Category\tVoxel Count\n")
+	for code, desc in categories.items():
+		n_voxels = np.sum(diff_volume == code)
+		f.write(f"{desc}\t{n_voxels}\n")
+
+# Save as diff_volume as .mgz
 new_header = img1.header.copy()
 new_header.set_data_dtype(np.int16)
 out_img = nib.MGHImage(diff_volume, img1.affine, header=new_header)
 out_img.to_filename(output_file)
-print(f'Saved output file {output_file}')
+
+print(f'Saved output files {output_file} and {counts_file_name}')
+
 

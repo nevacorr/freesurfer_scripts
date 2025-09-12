065231da01ba37fed36d312aaddba0935f5ec3a4
nevacorr <nevao@uw.edu>
2025-09-12 14:52:27 -0700

    initial commit

---
+++ overlay_parcellations_mpf_mprage/run_compare.sh
@@ -0,0 +1,28 @@
+cp run_compare.sh backup_run_compare.sh
+
+#python3 compare_parcellations_full_brain.py H05-2_mprage1_freesurfer/mri/aparc+aseg.mgz output/H05-2_MPFcor_aparc+aseg_reg_to_mprage.mgz
+
+#python3 compare_parcellations_full_brain.py H05-2_mprage1_freesurfer/mri/aparc+aseg.mgz output/H05-2_MPFcor_sqrt_aparc+aseg_reg_to_mprage.mgz
+
+#python3 compare_parcellations_full_brain.py H05-2_mprage1_1.25mm_freesurfer/mri/aparc+aseg.mgz output/H05-2_MPFcor_aparc+aseg_reg_to_mprage.mgz
+
+
+#python3 compare_parcellations_full_brain.py H08-2_mprage1_freesurfer/mri/aparc+aseg.mgz output/H08-2_MPFcor_aparc+aseg_reg_to_mprage.mgz
+
+#python3 compare_parcellations_full_brain.py H08-2_mprage1_freesurfer/mri/aparc+aseg.mgz output/H08-2_MPFcor_sqrt_aparc+aseg_reg_to_mprage.mgz
+
+#python3 compare_parcellations_full_brain.py H08-2_mprage1_1.25mm_freesurfer/mri/aparc+aseg.mgz output/H08-2_MPFcor_aparc+aseg_reg_to_mprage.mgz
+
+
+#python3 compare_parcellations_full_brain.py H10-2_mprage1_freesurfer/mri/aparc+aseg.mgz output/H10-2_MPFcor_aparc+aseg_reg_to_mprage.mgz
+
+#python3 compare_parcellations_full_brain.py H10-2_mprage1_freesurfer/mri/aparc+aseg.mgz output/H10-2_MPFcor_sqrt_aparc+aseg_reg_to_mprage.mgz
+
+#python3 compare_parcellations_full_brain.py H10-2_mprage1_1.25mm_freesurfer/mri/aparc+aseg.mgz output/H10-2_MPFcor_aparc+aseg_reg_to_mprage.mgz
+
+
+python3 compare_parcellations_full_brain.py H05-2_mprage1_freesurfer/mri/aparc+aseg.mgz output/H05-2_MPFcor_noskullstrip_aparc+aseg_reg_to_mprage.mgz
+
+python3 compare_parcellations_full_brain.py H08-2_mprage1_freesurfer/mri/aparc+aseg.mgz output/H08-2_MPFcor_noskullstrip_aparc+aseg_reg_to_mprage.mgz
+
+python3 compare_parcellations_full_brain.py H10-2_mprage1_freesurfer/mri/aparc+aseg.mgz output/H10-2_MPFcor_noskullstrip_aparc+aseg_reg_to_mprage.mgz

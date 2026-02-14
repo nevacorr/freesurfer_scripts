00853e6f235fa1e962e6bfa6686ab4ba121e8fbe
Neva <nevao@uw.edu>
2026-02-13 18:40:14 -0800

    use MPFreg as orig_mpf_file for calculating mean parcel value

--- make_lobe_masks/extract_mean_mpf_lobe_updated_antsreg.sh
+++ make_lobe_masks/extract_mean_mpf_lobe_updated_antsreg.sh
@@ -6,11 +6,13 @@
 # This script computes mean MPF values for each region in wmparc.mgz from 1) the fs parcellation of the MPF (volumes
 # generated after registering component images), while excluding all MPF voxels with
 # values below 200 and 2) wmparc.mgz from the fs parcellation of the MPRAGE, while excluding all MPF voxels with values below 200
+# and 3) the wmparc.mgz from the fs parcellation of the MPF where the component images were not registered prior to MPF reconstruction
 
 set -e #stop on errors
 
-FS_DIR_MPF="newrecon_reg_to_PD/freesurfer_output" # directory with new mpf all PD reg fs processed output
-FS_DIR_MPRAGE="freesurfer_output" 	          # directory with mprage fs processed output
+FS_DIR_MPF="/home/toddr/neva/MPF/parcellate_MPF_MPRAGE_v8.0/freesurfer_output"
+FS_DIR_MPF_REG="/home/toddr/neva/MPF/parcellate_MPF_MPRAGE_v8.0/newrecon_reg_to_PD/freesurfer_output" # directory with new mpf all PD reg fs processed output
+FS_DIR_MPRAGE="/home/toddr/neva/MPF/parcellate_MPF_MPRAGE_v8.0/freesurfer_output" 	          # directory with mprage fs processed output
 MASKS_DIR="combined_masks_Feb2026"
 REGIONS_LIST="allregions.txt"
 
@@ -19,239 +21,158 @@ TEMP_DIR="./temp_custom_region_files" 		  # directory to write intermediate file
 
 mkdir -p "$OUTPUT_DIR" "$TEMP_DIR"
 
-mean_mpf_mpfreg="$OUTPUT_DIR/allsubjects_mpf_mean_mpfregspace.csv"
-sd_mpf_mpfreg="$OUTPUT_DIR/allsubjects_mpf_sd_mpfregspace.csv"
+mean_mpf_mpf="$OUTPUT_DIR/allsubjects_mpf_mean_mpfspace.csv"
+sd_mpf_mpf="$OUTPUT_DIR/allsubjects_mpf_sd_mpfspace.csv"
+mean_mpf_mpf_reg="$OUTPUT_DIR/allsubjects_mpf_mean_mpfregspace.csv"
+sd_mpf_mpf_reg="$OUTPUT_DIR/allsubjects_mpf_sd_mpfregspace.csv"
 mean_mpf_mprage="$OUTPUT_DIR/allsubjects_mpf_mean_mpragespace.csv"
 sd_mpf_mprage="$OUTPUT_DIR/allsubjects_mpf_sd_mpragespace.csv"
 
-# Flags to write headers only once
-header_written_mpf=false
-header_written_mprage=false
+#Loop over all subject directories
 
-# Loop over all mpf directories
-for mpf_top_dir in "$FS_DIR_MPF"/H??-?_reg_MPFcor_freesurfer; do    # for every directory with MPF reg data
+for space in mpf mpf_reg mprage; do
 
-	subj_id=$(basename "$mpf_top_dir" | sed 's/_reg_MPFcor_freesurfer//')  # extract subject ID
-	mprage_top_dir="${FS_DIR_MPRAGE}/${subj_id}_mprage1_freesurfer"        # find MPRAGE fs processed output for this subject
+	if [[ "$space" == "mpf" ]]; then
+		fs_dir="$FS_DIR_MPF"
+		suffix="_MPFcor_freesurfer"
+		mean_output="$mean_mpf_mpf"
+		sd_output="$sd_mpf_mpf"
 
-	mpf_dir="$mpf_top_dir/mri"            		# path to mri directory in subject's MPF fs output
-	mprage_dir="$mprage_top_dir/mri"      		# path to mri directory in subject's MPRAGE fs output
-	wmseg_file_mpf="$mpf_dir/wmparc.mgz"      	# full path to wmparc.mgz seg file from MPF fs output
-	wmseg_file_mprage="$mprage_dir/wmparc.mgz"     	# full path to wmparc.mgz seg file from MPRAGE fs output
-	brain_fs_file_mpf="$mpf_dir/brain.mgz"     	# full path to brain.mgz file from MPF fs output
-	brain_fs_file_mprage="$mprage_dir/brain.mgz"   	# full path to brain.mgz file from MPRAGE fs output
-	orig_mpf_file="$mpf_dir/orig/001.mgz" 		# full path to 001.mgz file from MPF fs output
+	elif [[ "$space" == "mpf_reg" ]]; then
+		fs_dir="$FS_DIR_MPF_REG"
+		suffix="_reg_MPFcor_freesurfer"
+		mean_output="$mean_mpf_mpf_reg"
+		sd_output="$sd_mpf_mpf_reg"
 
-	echo ""
-	echo "---------------------"
-	echo "Processing $subj_id"
+	elif [[ "$space" == "mprage" ]]; then
+		fs_dir="$FS_DIR_MPRAGE"
+		suffix="_mprage1_freesurfer"
+		mean_output="$mean_mpf_mprage"
+		sd_output="$sd_mpf_mpf_mprage"
+	fi
+	
+	pattern="$fs_dir/H??-?${suffix}"
 
-	# Use MPF parcellation
-	if [[ -f "$wmseg_file_mpf" ]]; then
-		echo " ---------> Registering to MPF space"
+	echo "Pattern for $space: $pattern"
 
-		coreg_nii_mpf="${TEMP_DIR}/${subj_id}_001_in_mpf_wmparc.nii.gz"             # location of MPF 001 in MPF wmparc space	
-		coreg_nii_binary_mpf="${TEMP_DIR}/${subj_id}_001_in_mpf_wmparc_mask.nii.gz" # location of mask where MPF 001 in mpf wmparc space has values >200	
-		coreg_mgz_mpf="${TEMP_DIR}/${subj_id}_001_in_mpf_wmparc.mgz"          	    # location of MPF 001 in MPF wmparc space	
-		coreg_mgz_binary_mpf="${TEMP_DIR}/${subj_id}_001_in_mpf_wmparc_mask.mgz"    # location of mask where MPF 001 in mpf wmparc space has values >200	
+	subjects=($fs_dir/H??-?${suffix})
 
-		if [[ ! -f "$coreg_mgz_binary_mpf" ]]; then
+        for subj_dir in ${subjects[@]}; do
 
-			# Create prefix for ANTS registration output
-			ants_prefix="${TEMP_DIR}/${subj_id}_mpf2mpf"
+		subj_id=$(basename "$subj_dir" | sed "s/${suffix}//")
 
-			mpf_nii="${TEMP_DIR}/${subj_id}_001_for_mpf_wmparc_coreg.nii.gz"
-			brain_mpf_nii="${TEMP_DIR}/${subj_id}_brain_mpf.nii.gz"
+		subject_top_dir="${fs_dir}/${subj_id}${suffix}"
+		mri_dir="$subject_top_dir/mri"
+		wmseg_file="$mri_dir/wmparc.mgz"      	# full path to wmparc.mgz seg file from fs output
+		brain_fs_file="$mri_dir/brain.mgz"     	# full path to brain.mgz file from fs output
+		orig_mpf_file="$FS_DIR_MPF_REG/${subj_id}_reg_MPFcor_freesurfer/mri/orig/001.mgz"   # path to MPF reg file
 
-			echo "Convert input images from MGZ to NIFTI"
-			mri_convert "$orig_mpf_file" "$mpf_nii"
-			mri_convert "$brain_fs_file_mpf" "$brain_mpf_nii"
+		echo ""
+		echo "---------------------"
+		echo "Processing $subj_id in $space space"
 
-			# Perform rigid registration of mpf file  to mpf brain.mgz			
-			antsRegistration -d 3 \
-				-r "[$brain_mpf_nii,$mpf_nii,1]" \
-				-m "MI[$brain_mpf_nii,$mpf_nii,1,32]"  \
-				-t "Rigid[0.1]"  \
-				-c "[1000x500x250,1e-6,10]" \
-				-s "4x2x1vox" \
-				-f "8x4x2" \
-				-o "${ants_prefix}_Rigid_"
+		if [[ -f "$wmseg_file" ]]; then
+			echo " ---------> Registering to ${space} space"
 
-			# Perform affine registration of mpf file to mpf brain.mgz
-			antsRegistration -d 3 \
-				-r "[${ants_prefix}_Rigid_0GenericAffine.mat,1]"  \
-				-m "MI[$brain_mpf_nii,$mpf_nii,1,32]" \
-				-t "Affine[0.1]" \
-				-c "[1000x500x250,1e-6,10]" \
-				-s "4x2x1vox" \
-				-f "8x4x2" \
-				-o "${ants_prefix}_Affine_"
+			prefix="${TEMP_DIR}/${subj_id}_${space}"
 
-			# Apply final transform to original MPF to bring it into brain.mgz / wmparc space
-			antsApplyTransforms -d 3 \
-				-i "$mpf_nii" \
-				-r "$brain_mpf_nii" \
-				-o "$coreg_nii_mpf" \
-				-t "${ants_prefix}_Affine_0GenericAffine.mat" \
-				-n Linear
+			coreg_nii="${prefix}_001_in_wmparc.nii.gz"             # location of 001 in wmparc space	
+			coreg_nii_binary="${prefix}_001_in_wmparc_mask.nii.gz" # location of mask where 001 in wmparc space has values >200	
+			coreg_mgz="${prefix}_001_in_wmparc.mgz"                # location of 001 in wmparc space	
+			coreg_mgz_binary="${prefix}_001_in_wmparc_mask.mgz"    # location of mask where 001 in wmparc space has values >200	
 
-			echo "Check transformed output file"
-			ls -lh "$coreg_nii_mpf"
+			if [[ ! -f "$coreg_mgz_binary" ]]; then
 
-			# Make a binary mask of the MPF 001.mgz image that is in MPF wmparc space
-			mri_binarize --i "$coreg_nii_mpf" --min 200 --o "$coreg_nii_binary_mpf"  
+				# Create prefix for ANTS registration output
+				ants_prefix="${prefix}_ants"
 
-			mri_convert "$coreg_nii_mpf" "$coreg_mgz_mpf"
-			mri_convert "$coreg_nii_binary_mpf" "$coreg_mgz_binary_mpf"
+				mpf_nii="${prefix}_001_for_wmparc_coreg.nii.gz"
+				brain_nii="${prefix}_brain.nii.gz"
 
-		else 
-			echo " -_-------> Registration and masking for MPF already done, skipping"
-		fi
+				echo "Convert input images from MGZ to NIFTI"
+				mri_convert "$orig_mpf_file" "$mpf_nii"
+				mri_convert "$brain_fs_file" "$brain_nii"
 
+				# Perform rigid registration of mpf file to mri brain.mgz			
+				antsRegistration -d 3 \
+					-r "[$brain_nii,$mpf_nii,1]" \
+					-m "MI[$brain_nii,$mpf_nii,1,32]"  \
+					-t "Rigid[0.1]"  \
+					-c "[1000x500x250,1e-6,10]" \
+					-s "4x2x1vox" \
+					-f "8x4x2" \
+					-o "${ants_prefix}_Rigid_"
 
-		# Extract headers and values
-		if ! $header_written_mpf; then
-			headers=$(awk '{printf ",%s", $1} END {print ""}' "$REGIONS_LIST")
-			echo "Subject$headers" > "$mean_mpf_mpfreg"
-			echo "Subject$headers" > "$sd_mpf_mpfreg"
-			header_written_mpf=true
-		fi
+				# Perform affine registration of mpf file to mri brain.mgz
+				antsRegistration -d 3 \
+					-r "[${ants_prefix}_Rigid_0GenericAffine.mat,1]"  \
+					-m "MI[$brain_nii,$mpf_nii,1,32]" \
+					-t "Affine[0.1]" \
+					-c "[1000x500x250,1e-6,10]" \
+					-s "4x2x1vox" \
+					-f "8x4x2" \
+					-o "${ants_prefix}_Affine_"
 
-		mean_vals=""
-		sd_vals=""
+				# Apply final transform to original mpf to bring it into brain.mgz / wmparc space
+				antsApplyTransforms -d 3 \
+					-i "$mpf_nii" \
+					-r "$brain_nii" \
+					-o "$coreg_nii" \
+					-t "${ants_prefix}_Affine_0GenericAffine.mat" \
+					-n Linear
 
-		# Calculate mean MPF value in each region
-		while read -r region; do 
-			region_mask="$MASKS_DIR/$subj_id/${region}.mgz"
+				echo "Check transformed output file"
+				ls -lh "$coreg_nii"
 
-			if [[ ! -f "$region_mask" ]]; then
-				mean_vals+=",NA"
-				sd_vals+=",NA"
-				continue
+				# Make a binary mask of the MPF 001.mgz image that is in wmparc space
+				mri_binarize --i "$coreg_nii" --min 200 --o "$coreg_nii_binary"  
+
+				mri_convert "$coreg_nii" "$coreg_mgz"
+				mri_convert "$coreg_nii_binary" "$coreg_mgz_binary"
+
+			else 
+				echo " -_-------> Registration and masking for MPF already done, skipping"
 			fi
+
+
+			# Extract headers and values
+			if [[ ! -f "$mean_output" ]]; then
+				headers=$(awk '{printf ",%s", $1} END {print ""}' "$REGIONS_LIST")
+				echo "Subject$headers" > "$mean_output"
+				echo "Subject$headers" > "$sd_output"
+			fi
+
+			mean_vals=""
+			sd_vals=""
+
+			# Calculate mean MPF value in each region
+			while read -r region; do 
+				region_mask="$MASKS_DIR/$subj_id${suffix}/${region}.mgz"
+
+				if [[ ! -f "$region_mask" ]]; then
+					mean_vals+=",NA"
+					sd_vals+=",NA"
+					continue
+				fi
 			
 
-			tmp_mask="$TEMP_DIR/${subj_id}_${region}_mpfmask.mgz"
-			mri_and "$region_mask" "$coreg_mgz_binary_mpf" "$tmp_mask"
+				tmp_mask="${prefix}_${region}_mask.mgz"
+				mri_and "$region_mask" "$coreg_mgz_binary" "$tmp_mask"
 
-			stats=$(mri_stats --mask "$tmp_mask" --i "$coreg_mgz_mpf" --mean --std 2>/dev/null)
+				stats=$(mri_stats --mask "$tmp_mask" --i "$coreg_mgz" --mean --std 2>/dev/null)
 
-			mean=$(echo "$stats" | awk '{print $1}')
-			sd=$(echo "$stats" | awk '{print $2}')
+				mean=$(echo "$stats" | awk '{print $1}')
+				sd=$(echo "$stats" | awk '{print $2}')
 
-			mean_vals+=",$mean"
-			sd_vals+=",$sd"
+				mean_vals+=",$mean"
+				sd_vals+=",$sd"
 		
-		done < "$REGIONS_LIST"	
+			done < "$REGIONS_LIST"	
 	
-		echo "$subj_id$mean_vals" >>"$mean_mpf_mpfreg"
-		echo "$subj_id$sd_vals" >> "$sd_mpf_mpfreg"
-
-	else
-		echo "Skipping $subj_id for MPF parcellation (missing wmparc.mgz for MPF)"
-	fi
-		
-	# Use MPRAGE parcellation
-	if [[ -f "$wmseg_file_mprage" ]]; then
-		echo " ---------> Registering to MPRAGE space"
-
-		coreg_nii_mprage="${TEMP_DIR}/${subj_id}_001_in_mprage_wmparc.nii.gz"    	# location of MPF 001 in MPRAGE wmparc space	
-		coreg_nii_binary_mprage="${TEMP_DIR}/${subj_id}_001_in_mprage_wmparc_mask.nii.gz" # location of mask where MPF 001 in mprage wmparc space has values >200	
-		coreg_mgz_mprage="${TEMP_DIR}/${subj_id}_001_in_mprage_wmparc.mgz"    		# location of MPF 001 in MPRAGE wmparc space	
-		coreg_mgz_binary_mprage="${TEMP_DIR}/${subj_id}_001_in_mprage_wmparc_mask.mgz"  # location of mask where MPF 001 in mprage wmparc space has values >200	
-		
-		if [[ ! -f "$coreg_mgz_binary_mprage" ]]; then
-
-			# Create prefix for ANTS registration output
-			ants_prefix="${TEMP_DIR}/${subj_id}_mpf2mprage"
-
-			mpf_nii="${TEMP_DIR}/${subj_id}_001_for_mprage_wmparc_coreg.nii.gz"
-			brain_mprage_nii="${TEMP_DIR}/${subj_id}_brain_mprage.nii.gz"
-
-			echo "Convert input images from MGZ to NIFTI"
-			mri_convert "$orig_mpf_file" "$mpf_nii"
-			mri_convert "$brain_fs_file_mprage" "$brain_mprage_nii"
-
-			# Perform rigid registration of mpf file  to mprage brain.mgz			
-			antsRegistration -d 3 \
-				-r "[$brain_mprage_nii,$mpf_nii,1]" \
-				-m "MI[$brain_mprage_nii,$mpf_nii,1,32]"  \
-				-t "Rigid[0.1]"  \
-				-c "[1000x500x250,1e-6,10]" \
-				-s "4x2x1vox" \
-				-f "8x4x2" \
-				-o "${ants_prefix}_Rigid_"
-
-			# Perform affine registration of mpf file to mprage brain.mgz
-			antsRegistration -d 3 \
-				-r "[${ants_prefix}_Rigid_0GenericAffine.mat,1]"  \
-				-m "MI[$brain_mprage_nii,$mpf_nii,1,32]" \
-				-t "Affine[0.1]" \
-				-c "[1000x500x250,1e-6,10]" \
-				-s "4x2x1vox" \
-				-f "8x4x2" \
-				-o "${ants_prefix}_Affine_"
-
-			# Apply final transform to original MPF to bring it into brain.mgz / wmparc space
-			antsApplyTransforms -d 3 \
-				-i "$mpf_nii" \
-				-r "$brain_mprage_nii" \
-				-o "$coreg_nii_mprage" \
-				-t "${ants_prefix}_Affine_0GenericAffine.mat" \
-				-n Linear
-
-			# Make a binary mask of the MPF values > 200 
-			mri_binarize --i "$coreg_nii_mprage" --min 200 --o "$coreg_nii_binary_mprage" 
-
-			mri_convert "$coreg_nii_mprage" "$coreg_mgz_mprage"
-			mri_convert "$coreg_nii_binary_mprage" "$coreg_mgz_binary_mprage"
-
-		else 
-			echo " --------> Registration and masking for MPRAGE already done, skipping"
+			echo "$subj_id$mean_vals" >>"$mean_output"
+			echo "$subj_id$sd_vals" >> "$sd_output"
 		fi
-
-		# Extract headers and values
-		if ! $header_written_mprage; then
-			headers=$(awk '{printf ",%s", $1} END {print ""}' "$REGIONS_LIST") 
-			echo "Subject$headers" > "$mean_mpf_mprage"
-			echo "Subject$headers" > "$sd_mpf_mprage"
-			header_written_mprage=true
-		fi
-
-		mean_vals=""
-		sd_vals=""
-
-		while read -r region; do
-			region_mask="$MASKS_DIR/$subj_id/${region}.mgz"
-
-			if [[ ! -f "$region_mask" ]]; then
-				mean_vals+=",NA"
-				sd_vals+=",NA"
-				continue
-			fi
-
-			tmp_mask="$TEMP_DIR/${subj_id}_${region}_mpragemask.mgz"
-			mri_and "$region_mask" "$coreg_mgz_binary_mprage" "$tmp_mask"
-
-			# Calculate mean MPF value using MPRAGE parcellation
-			stats=$(mri_stats --mask "$tmp_mask" --i "$coreg_mgz_mprage" --mean --std 2>/dev/null)
-
-			mean=$(echo "$stats" | awk '{print $1}')
-			sd=$(echo "$stats" | awk '{print $2}')
-
-			mean_vals+=",$mean"
-			sd_vals+=",$sd"
-
-		done < "$REGIONS_LIST"
-		
-		echo "$subj_id$mean_vals" >>"$mean_mpf_mprage"
-		echo "$subj_id$sd_vals" >> "$sd_mpf_mprage"
-
-
-	else
-		echo "Skipping $subj_id calculation for MPRAGE parcellation (missing wmparc.mgz for MPRAGE)"
-
-	fi
+	done
 done
 
 echo "Calculations complete. All results are saved in $OUTPUT_DIR"
